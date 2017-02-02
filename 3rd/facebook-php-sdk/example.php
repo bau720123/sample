1d545a118aeacb00f1ca0204277b6d87699d592c
Colin <bau720123@gmail.com>
2017-02-02 16:21:12 +0800

    更新facebook的SDK，雖然更新的SDK也是舊的了

--- 3rd/facebook-php-sdk/example.php
+++ 3rd/facebook-php-sdk/example.php
@@ -27,7 +27,9 @@ if ($me) {
 } else {
   //$loginUrl = $facebook->getLoginUrl();
   $loginUrl = $facebook->getLoginUrl(array(
-    'fbconnect' => 1,
+    'scope' => 'email,user_birthday,user_friends', 
+	'auth_type' => 'reauthenticate',
+	'fbconnect' => 1,
     'canvas' => 0,
 	'req_perms' => 'email,publish_stream',
     'next' => 'http://www.littlebau.com/index.php',
@@ -37,7 +39,7 @@ if ($me) {
 $home = explode(", ", $me['hometown']['name']);
 
 // 取得登入使用者 ID
-$user = $facebook -> getUser();
+$user = $facebook->getUser();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html 
@@ -45,8 +47,6 @@ xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>無標題文件</title>
-<script type="text/javascript" src="javascript/jquery.min.js"></script>
-<script type="text/javascript" src="javascript/javascript.js"></script>	
 </head>
 
 <body>
@@ -76,42 +76,32 @@ xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml
     </script><?php if ($me['verified'] =='') { ?><fb:login-button>登入</fb:login-button>
     <?php } ?><?php if ($me['verified'] =='1') { ?>
     <a href="<?php echo $facebook->getLogoutUrl(); ?>">登出</a><? } ?><br/>
-    大頭貼：<img src="https://graph.facebook.com/<?php echo $uid; ?>/picture" /><br />
-    會員編號：<?php print_r($session); ?><br />
+
+大頭貼：<img src="https://graph.facebook.com/<?php echo $uid; ?>/picture" /><br />
+會員編號：<?php print_r($session); ?> || <?php echo $me['id']; ?><br />
+生日：<?php echo $me['birthday']; ?><br />
+EMAIL：<?php echo $me['email']; ?><br />
+名：<?php echo $me['first_name']; ?><br /> 
+性別：<?php echo $me['gender']; ?><br />
 姓：<?php echo $me['last_name']; ?><br />
-姓：<?php echo $me['first_name']; ?><br /> 
+短網址：<?php echo $me['link']; ?><br />
+地理：<?php echo $me['locale']; ?><br />
 姓名：<?php echo $me['name']; ?><br />
 自我介紹：<?php echo $me['bio']; ?> <br />
-性別：<?php echo $me['gender']; ?><br />
 時區：<?php echo $me['timezone']; ?><br />
-地理：<?php echo $me['locale']; ?><br />
-使用語言：<?php echo $me['languages'][0]['name']; ?><br />
-認證狀態：<?php echo $me['verified']; ?><br />
 最新一次更新自己狀態時間：<?php echo $me['updated_time']; ?><br />
-短網址：<?php echo $me['link']; ?><br />
-帳號：<?php echo $me['username']; ?><br />
-家鄉：<?php echo $home[1]; ?><br />
-城市：<?php echo $home[0]; ?><br />
-喜愛的佳言絕句：<?php echo $me['quotes']; ?><br />
-<br />
-高中：<?php echo $me['education'][0]['school']['name']; ?><br />
-什麼時候畢業的：<?php echo $me['education'][0]['year']['name']; ?><br />
-科系：<?php echo $me['education'][0]['classes'][0]['name']; ?><br />
-跟誰一起讀過這間學校：<?php echo $me['education'][0]['classes'][0]['with'][0]['name']; ?><br />
-介紹：<?php echo $me['education'][0]['classes'][0]['description']; ?><br />
-<br />
-大學：<?php echo $me['education'][1]['school']['name']; ?><br />
-什麼時候畢業的：<?php echo $me['education'][1]['year']['name']; ?><br />
-科系：<?php echo $me['education'][1]['classes'][0]['name']; ?><br />
-跟誰一起讀過這間學校：<?php echo $me['education'][1]['classes'][0]['with'][0]['name']; ?><br />
-介紹：<?php echo $me['education'][1]['classes'][0]['description']; ?><br />
+認證狀態：<?php echo $me['verified']; ?><br />
 好友名單:<br />
 <?php
 if ($user) {
-    $user_profile = $facebook->api('/me');
-    $friends = $facebook->api('/me/friends');
-
-	echo '<ul>';
+    //$user_profile = $facebook->api('/me');
+	
+	$friends = $facebook->api('/me/friends');
+    
+	echo '<pre>';
+	print_r($friends);
+	echo '</pre>';
+	/*echo '<ul>';
     foreach ($friends["data"] as $value) {
         echo '<li>';
         echo '<div class="pic">';
@@ -120,9 +110,8 @@ if ($user) {
         echo '<div class="picName">'.$value["name"].'</div>'; 
         echo '</li>';
     }
-    echo '</ul>';
+    echo '</ul>';*/
 }
 ?>
-<pre><?php print_r($me); ?></pre>
 </body>
 </html>
