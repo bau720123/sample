96423aa667394a4833829b4c3896d7ededa33d32
Colin <bau720123@gmail.com>
2017-05-16 10:45:46 +0800

    更新

--- php.php
+++ php.php
@@ -697,5 +697,8 @@ if($archive->extract() == 0)
 die("錯誤︰".$archive->errorInfo(true)); 
 }*/
 ?>
+<?php
+//copy('https://s3-ap-northeast-1.amazonaws.com/tvbs20160226/img/drama/youthpower/images/98130bc8b1779cee8d28efbabfc4a2f0.jpg', $dest.'98130bc8b1779cee8d28efbabfc4a2f0.jpg'); //另外一種變通的方法
+?>
 </body>
 </html>
